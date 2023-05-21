5854f2f4db66f10b10d625be26bd74236a1bc460
takahiro_aoki <takahiro.aoki.0902@gmail.com>
2023-05-21 13:33:01 +0900

    prepare env

---
+++ webapimock/src/main/java/com/noname/webapimock/WebapimockApplication.java
@@ -0,0 +1,13 @@
+package com.noname.webapimock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebapimockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebapimockApplication.class, args);
+	}
+
+}
---
+++ webapimock/src/test/java/com/noname/webapimock/WebapimockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noname.webapimock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebapimockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
