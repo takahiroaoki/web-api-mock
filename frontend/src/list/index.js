2baf5d89f071dbbf301b349a81bfc1daab07a573
takahiro_aoki <takahiro.aoki.0902@gmail.com>
2023-05-24 20:51:31 +0900

    overlay

--- frontend/src/list/index.js
+++ frontend/src/list/index.js
@@ -1,13 +1,18 @@
 import CreateButton from './components/create-button/create-button';
+import Form from './components/form/form';
 import './index.scss';
 
 const index = () => {
     // mount components
     const createButton = new CreateButton('create-button');
+    const form = new Form('form');
 
     // setting
     createButton.onClick(() => {
-        console.log("The button has been clicked.");
+        form.show();
+    });
+    form.onClickCloseButton(() => {
+        form.hide();
     });
 };
 
